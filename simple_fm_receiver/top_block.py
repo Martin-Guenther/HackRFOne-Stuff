aeaf4de5df37f8b434f5ae3d8dd09a6fe3875164
Martin-Guenther <martin.guenther@outlook.de>
2019-03-23 18:46:04 +0100

    simple_fm_receiver/feat: Add more plots and split low pass filter block
    Added waterfall plot and second fft plot. Also splitted the low pass
    filter block, that additionally decimates the signal into a resampler
    block and a low pass filter block to show clearer what happens.

--- simple_fm_receiver/top_block.py
+++ simple_fm_receiver/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Wed Mar  6 09:47:52 2019
+# Generated: Sat Mar 23 18:45:17 2019
 ##################################################
 
 from distutils.version import StrictVersion
@@ -71,37 +71,84 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.volume = volume = 1
-        self.transition = transition = 1e6
         self.samp_rate = samp_rate = 10e6
         self.quadrature_rate = quadrature_rate = 500e3
-        self.freq = freq = 106.4e6
-        self.cutoff = cutoff = 100e3
+        self.volume = volume = 1
+        self.trans = trans = 100e3
+        self.freq = freq = 105.4e6
+        self.decimation = decimation = int(samp_rate / quadrature_rate)
+        self.cut_off = cut_off = 200e3
+        self.cos_freq = cos_freq = 0
         self.audio_sample_rate = audio_sample_rate = 32e3
 
         ##################################################
         # Blocks
         ##################################################
-        self._volume_range = Range(0, 10, 1, 1, 200)
+        self._volume_range = Range(0, 15, 0.1, 1, 200)
         self._volume_win = RangeWidget(self._volume_range, self.set_volume, 'Volume', "counter_slider", float)
         self.top_layout.addWidget(self._volume_win)
-        self._transition_range = Range(10e3, 2e6, 100, 1e6, 200)
-        self._transition_win = RangeWidget(self._transition_range, self.set_transition, 'Transition', "counter_slider", float)
-        self.top_layout.addWidget(self._transition_win)
-        self._cutoff_range = Range(1e3, 200e3, 100, 100e3, 200)
-        self._cutoff_win = RangeWidget(self._cutoff_range, self.set_cutoff, 'Cutoff', "counter_slider", float)
-        self.top_layout.addWidget(self._cutoff_win)
+        self._trans_range = Range(10e3, 200e3, 10e3, 100e3, 200)
+        self._trans_win = RangeWidget(self._trans_range, self.set_trans, 'Transition', "counter_slider", float)
+        self.top_layout.addWidget(self._trans_win)
+        self._cut_off_range = Range(50e3, 250e3, 20e3, 200e3, 200)
+        self._cut_off_win = RangeWidget(self._cut_off_range, self.set_cut_off, 'Cut Off', "counter_slider", float)
+        self.top_layout.addWidget(self._cut_off_win)
+        self._cos_freq_range = Range(-2e6, 2e6, 10e3, 0, 200)
+        self._cos_freq_win = RangeWidget(self._cos_freq_range, self.set_cos_freq, 'Cos Freq', "counter_slider", float)
+        self.top_layout.addWidget(self._cos_freq_win)
+        self.rational_resampler_xxx_1 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=decimation,
+                taps=None,
+                fractional_bw=None,
+        )
         self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
                 interpolation=int(audio_sample_rate / 1000),
                 decimation=int(quadrature_rate / 1000),
                 taps=None,
                 fractional_bw=None,
         )
+        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
+        	1024, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	freq - cos_freq, #fc
+        	samp_rate  / decimation , #bw
+        	"", #name
+                1 #number of inputs
+        )
+        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
+
+        if not True:
+          self.qtgui_waterfall_sink_x_0.disable_legend()
+
+        if "complex" == "float" or "complex" == "msg_float":
+          self.qtgui_waterfall_sink_x_0.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
         self.qtgui_freq_sink_x_0_0 = qtgui.freq_sink_c(
         	1024, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
         	0, #fc
-        	samp_rate, #bw
+        	samp_rate / decimation, #bw
         	"after lowpass", #name
         	1 #number of inputs
         )
@@ -143,7 +190,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
         	1024, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
+        	freq - cos_freq, #fc
         	samp_rate, #bw
         	"osmocom output", #name
         	1 #number of inputs
@@ -194,32 +241,58 @@ class top_block(gr.top_block, Qt.QWidget):
         self.osmosdr_source_0.set_if_gain(20, 0)
         self.osmosdr_source_0.set_bb_gain(20, 0)
         self.osmosdr_source_0.set_antenna('', 0)
-        self.osmosdr_source_0.set_bandwidth(1e6, 0)
+        self.osmosdr_source_0.set_bandwidth(2e6, 0)
 
-        self.low_pass_filter_0 = filter.fir_filter_ccf(int(samp_rate / quadrature_rate), firdes.low_pass(
-        	1, samp_rate, cutoff, transition, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate / decimation, cut_off, trans, firdes.WIN_HAMMING, 6.76))
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((volume, ))
         self.audio_sink_0 = audio.sink(int(audio_sample_rate), '', True)
         self.analog_wfm_rcv_0 = analog.wfm_rcv(
         	quad_rate=quadrature_rate,
         	audio_decimation=1,
         )
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, cos_freq, 1, 0)
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
         self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.rational_resampler_xxx_1, 0))
         self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.qtgui_freq_sink_x_0_0, 0))
-        self.connect((self.osmosdr_source_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.osmosdr_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.rational_resampler_xxx_1, 0), (self.low_pass_filter_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_decimation(int(self.samp_rate / self.quadrature_rate))
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq - self.cos_freq, self.samp_rate  / self.decimation )
+        self.qtgui_freq_sink_x_0_0.set_frequency_range(0, self.samp_rate / self.decimation)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.freq - self.cos_freq, self.samp_rate)
+        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate / self.decimation, self.cut_off, self.trans, firdes.WIN_HAMMING, 6.76))
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+
+    def get_quadrature_rate(self):
+        return self.quadrature_rate
+
+    def set_quadrature_rate(self, quadrature_rate):
+        self.quadrature_rate = quadrature_rate
+        self.set_decimation(int(self.samp_rate / self.quadrature_rate))
 
     def get_volume(self):
         return self.volume
@@ -228,42 +301,46 @@ class top_block(gr.top_block, Qt.QWidget):
         self.volume = volume
         self.blocks_multiply_const_vxx_0.set_k((self.volume, ))
 
-    def get_transition(self):
-        return self.transition
+    def get_trans(self):
+        return self.trans
 
-    def set_transition(self, transition):
-        self.transition = transition
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
-
-    def get_samp_rate(self):
-        return self.samp_rate
-
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-        self.qtgui_freq_sink_x_0_0.set_frequency_range(0, self.samp_rate)
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
-
-    def get_quadrature_rate(self):
-        return self.quadrature_rate
-
-    def set_quadrature_rate(self, quadrature_rate):
-        self.quadrature_rate = quadrature_rate
+    def set_trans(self, trans):
+        self.trans = trans
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate / self.decimation, self.cut_off, self.trans, firdes.WIN_HAMMING, 6.76))
 
     def get_freq(self):
         return self.freq
 
     def set_freq(self, freq):
         self.freq = freq
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq - self.cos_freq, self.samp_rate  / self.decimation )
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.freq - self.cos_freq, self.samp_rate)
         self.osmosdr_source_0.set_center_freq(self.freq, 0)
 
-    def get_cutoff(self):
-        return self.cutoff
+    def get_decimation(self):
+        return self.decimation
 
-    def set_cutoff(self, cutoff):
-        self.cutoff = cutoff
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+    def set_decimation(self, decimation):
+        self.decimation = decimation
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq - self.cos_freq, self.samp_rate  / self.decimation )
+        self.qtgui_freq_sink_x_0_0.set_frequency_range(0, self.samp_rate / self.decimation)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate / self.decimation, self.cut_off, self.trans, firdes.WIN_HAMMING, 6.76))
+
+    def get_cut_off(self):
+        return self.cut_off
+
+    def set_cut_off(self, cut_off):
+        self.cut_off = cut_off
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate / self.decimation, self.cut_off, self.trans, firdes.WIN_HAMMING, 6.76))
+
+    def get_cos_freq(self):
+        return self.cos_freq
+
+    def set_cos_freq(self, cos_freq):
+        self.cos_freq = cos_freq
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq - self.cos_freq, self.samp_rate  / self.decimation )
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.freq - self.cos_freq, self.samp_rate)
+        self.analog_sig_source_x_0.set_frequency(self.cos_freq)
 
     def get_audio_sample_rate(self):
         return self.audio_sample_rate
